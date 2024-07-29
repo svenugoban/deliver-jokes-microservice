e263ed197d5f9461eb3444d1b1d947f835a79be7
svenugoban <asvenugoban19962015@gmail.com>
2024-07-29 23:49:30 +0530

    Create a service to fetch jokes.

--- deliver-jokes-microservice/src/app.service.ts
+++ deliver-jokes-microservice/src/app.service.ts
@@ -1,8 +1,19 @@
+/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private jokes = {
+    dad: ['Dad joke 1', 'Dad joke 2'],
+    pun: ['Pun joke 1', 'Pun joke 2'],
+  };
+
+  getRandomJoke(type: string): string {
+    const jokes = this.jokes[type] || [];
+    return jokes[Math.floor(Math.random() * jokes.length)];
+  }
+
+  getJokeTypes(): string[] {
+    return Object.keys(this.jokes);
   }
 }
